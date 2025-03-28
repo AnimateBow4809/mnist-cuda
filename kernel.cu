b1a03c957e80030f4bea3c9344fd705b1c7f5835
AnimateBow4809 <alidashtbozorg709@gmail.com>
2025-03-28 22:05:22 +0330

    added some ux

--- kernel.cu
+++ kernel.cu
@@ -152,7 +152,7 @@ int main() {
     
     int choice = 0;
     while (true) {
-        printf("Enter your choice: ");
+        printf("Enter your choice:\n1. Train 10 epochs\n2. Run Through Tests\n3. Save Weights To file\n4. Show minimum and mazimum weights");
 
         // Check if scanf successfully reads an integer
         if (scanf("%d", &choice) != 1) {
